9b67ed6107e8683b8a4db96a9e8b7e92799d540d
Adam Gregořica <agregorica@gmail.com>
2025-09-23 20:50:23 +0200

    Added github logo into the top right of the page

--- index.php
+++ index.php
@@ -7,7 +7,12 @@
     <title>Code Golf</title>
 </head>
 <body>
+    <header>
+        <a href="https://github.com/Gregstr05"><img src="assets/images/github-mark-white.svg" height="20" alt="Github"></a>
+    </header>
+
     <h1>C Code Golf</h1>
-    
+
+    <p>You can check out the rules <a href=https://github.com/Gregstr05/c-code-golf>here</a></p>
 </body>
 </html>
